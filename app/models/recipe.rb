da79092db6f736cc5176177a8de6127f46143c51
RileyManda <rileymanda69@gmail.com>
2023-10-16 15:29:39 +0200

    Updated recipe model with table self

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
+  self.table_name = "recipe"
   belongs_to :user
   validates :name, presence: true
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
