a4d13d1927c18bc7cf0680116cfffc2aaf8f1ef8
RileyManda <rileymanda69@gmail.com>
2023-10-16 14:10:23 +0200

    Updated recipes models and controllers

--- app/controllers/recipe_food_controller.rb
+++ app/controllers/recipe_food_controller.rb
@@ -1,2 +1,3 @@
 class RecipeFoodController < ApplicationController
+def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root 'food#index'
 
   resources :food, only: [:index, :new, :create] do
-    resources :recipes, only: [:index, :new, :create]
+    resources :recipe, only: [:index, :new, :create]
   end
 
   resources :users, only: [:index, :show, :new, :create]
   resources :recipe_food, only: [:index, :new, :create]
-  resources :recipes, only: [:index, :new, :create]
+  resources :recipe, only: [:index, :new, :create]
 end
