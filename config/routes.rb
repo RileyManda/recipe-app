616b326f0b0f800f26b7de9a32bec2cde59ecdc5
RileyManda <rileymanda69@gmail.com>
2023-10-16 13:31:24 +0200

    Created routes for views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # config/routes.rb
 root 'foods#index'
-
+resources :foods, only: [:index, :new, :create]
+resources :users, only: [:index, :show, :new, :create]
+resources :recipe_foods, only: [:index, :new, :create]
+resources :recipes, only: [:index, :new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
