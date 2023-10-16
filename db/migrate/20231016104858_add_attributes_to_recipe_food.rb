5eeb0316ba8ec4393fe4f09c6df32605859b2431
RileyManda <rileymanda69@gmail.com>
2023-10-16 13:21:59 +0200

    Migrations:Models and Controller updates wioth params

--- db/migrate/20231016104858_add_attributes_to_recipe_food.rb
+++
@@ -1,7 +0,0 @@
-class AddAttributesToRecipeFood < ActiveRecord::Migration[7.0]
-  def change
-    add_column :recipe_foods, :quantity, :integer
-    add_reference :recipe_foods, :recipe, null: false, foreign_key: true
-    add_reference :recipe_foods, :food, null: false, foreign_key: true
-  end
-end
