d1827d6f5d8c2b03b243ec5609ee7c63cc8d3598
RileyManda <rileymanda69@gmail.com>
2023-10-16 15:57:47 +0200

    Bug fix:Delete recipe

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -6,4 +6,11 @@ class RecipeController < ApplicationController
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
   end
+
+  def destroy
+  @recipe = Recipe.find(params[:id])
+  @recipe.destroy
+  redirect_to recipe_path, notice: "Recipe was successfully deleted."
+end
+
 end
