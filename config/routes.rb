4baf0afff328bb0e1dd592eaba5951434b9c500f
RileyManda <rileymanda69@gmail.com>
2023-10-16 15:52:22 +0200

    Implemented recipe remove button

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :food, only: [:index, :new, :create] do
     resources :recipe, only: [:index, :new, :create]
   end
-
+  delete 'recipes/:id', to: 'recipes#destroy', as: 'remove_recipe'
   resources :users, only: [:index, :show, :new, :create]
   resources :recipe_food, only: [:index, :new, :create]
   resources :recipe, only: [:index, :new, :create]
