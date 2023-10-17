f10a772da9b5aec6d54fb84768d88fdb068ccbe3
RileyManda <rileymanda69@gmail.com>
2023-10-17 21:09:54 +0200

    Implemented engredients option

--- app/controllers/recipe_food_controller.rb
+++ app/controllers/recipe_food_controller.rb
@@ -7,3 +7,19 @@ end
 def show
   @recipe_food = RecipeFood.find(params[:id])
 end
+
+def create
+    @recipe_food = RecipeFood.new(recipe_food_params)
+
+    if @recipe_food.save
+      redirect_to your_recipe_path, notice: 'Ingredient added to recipe.'
+    else
+
+    end
+
+  private
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:food_id, :quantity, :recipe_id)
+  end
+end
