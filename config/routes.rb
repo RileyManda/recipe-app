31a48355c8fa8237eec4bfdbe034336419892a03
RileyManda <rileymanda69@gmail.com>
2023-10-17 11:57:10 +0200

    Bug fix:recipe index path

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root 'food#index'
 
   resources :food, only: [:index, :new, :create] do
-    resources :recipe, only: [:index, :new, :create]
+    resources :recipe, controller: 'recipe'
   end
-  delete 'recipes/:id', to: 'recipes#destroy', as: 'remove_recipe'
+  resources :recipe, only: [:index]
+  delete 'recipe/:id', to: 'recipe#destroy', as: 'remove_recipe'
   resources :users, only: [:index, :show, :new, :create]
   resources :recipe_food, only: [:index, :new, :create]
   resources :recipe, only: [:index, :new, :create, :show, :update, :destroy]
-  get '/public_recipes', to: 'recipe#public_recipes', as: 'public_recipes'
-
+  get '/public_recipe', to: 'recipe#public_recipe', as: 'public_recipe'
 end
