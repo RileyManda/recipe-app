c15d5bf579f2f626de1513edc11cbfa45c11ac71
RileyManda <rileymanda69@gmail.com>
2023-10-18 20:29:17 +0200

    Rspec:recipe:is not valid without a user|is valid with valid attributes

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,1 +1,34 @@
 require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  describe Recipe, type: :model do
+    it 'is valid with valid attributes' do
+      user = User.create(name: 'johndoes', email: 'john@email.com', password: '123456')
+      recipe = Recipe.new(
+        name: 'Sample Recipe',
+        preparation_time: 30,
+        cooking_time: 60,
+        description: 'Recipe description',
+        public: true,
+        user:
+      )
+      expect(recipe).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      recipe = Recipe.new(name: nil)
+      expect(recipe).not_to be_valid
+    end
+
+    it 'is not valid without a user' do
+      recipe = Recipe.new(
+        name: 'MyRecipe',
+        preparation_time: 30,
+        cooking_time: 60,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        public: true
+      )
+      expect(recipe).not_to be_valid
+    end
+  end
+end
