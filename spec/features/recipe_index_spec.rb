a1758b267e68f30e4f615e8196e6e7bd6e155aa0
RileyManda <rileymanda69@gmail.com>
2023-10-18 23:46:24 +0200

    Linter fixes

--- spec/features/recipe_index_spec.rb
+++ spec/features/recipe_index_spec.rb
@@ -1,31 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature 'Recipe#index Page', type: :feature do
-  scenario 'user can add a new recipe' do
+  scenario 'authorized user can add a new recipe' do
     user = create(:user)
-    login_as(user, :scope => :user)
+    login_as(user, scope: :user)
     visit recipe_index_path
     click_button('Add Recipe')
 
     expect(page).to have_current_path(new_recipe_path)
   end
-#   scenario 'user can see the list of recipes' do
-#     user = create(:user)
-#     recipe = create(name: 'Vegetable Soup', description: 'A delicious chicken curry', user: user)
-#     visit recipe_index_path
-#     expect(page).to have_content('Recipes')
-#     expect(page).to have_content('Vegetable Soup')
-#     expect(page).to have_content('A delicious chicken curry')
-#     expect(page).to have_button('Remove')
-#   end
+  #   scenario 'user can see the list of recipes' do
+  #     user = create(:user)
+  #     recipe = create(name: 'Vegetable Soup', description: 'A delicious chicken curry', user: user)
+  #     visit recipe_index_path
+  #     expect(page).to have_content('Recipes')
+  #     expect(page).to have_content('Vegetable Soup')
+  #     expect(page).to have_content('A delicious chicken curry')
+  #     expect(page).to have_button('Remove')
+  #   end
 
-#   scenario 'user sees a message when there are no recipes available' do
-#     visit recipe_index_path
+  #   scenario 'user sees a message when there are no recipes available' do
+  #     visit recipe_index_path
 
-#     expect(page).to have_content('Recipes')
-#     expect(page).to have_content('No recipes available.')
-#     expect(page).not_to have_button('Remove')
-#   end
-
-
+  #     expect(page).to have_content('Recipes')
+  #     expect(page).to have_content('No recipes available.')
+  #     expect(page).not_to have_button('Remove')
+  #   end
 end
