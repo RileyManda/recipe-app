f5cd76669814935620193da6ac831ce500ac7dfc
RileyManda <rileymanda69@gmail.com>
2023-10-18 14:41:50 +0200

    Updated application controller path

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
-  # attr_accessor :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at,
-  #               :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :recipes, dependent: :destroy
   has_many :recipes
   has_many :foods
