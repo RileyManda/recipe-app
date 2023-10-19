08db375ecfc27a465a1738fa9c845278dd6e7be5
RileyManda <rileymanda69@gmail.com>
2023-10-19 07:57:40 +0200

    Itergration Spec:recipe:user can remove their recipe|user can add a new recipe

--- spec/features/recipe_index_spec.rb
+++ spec/features/recipe_index_spec.rb
@@ -6,24 +6,20 @@ RSpec.feature 'Recipe#index Page', type: :feature do
     login_as(user, scope: :user)
     visit recipe_index_path
     click_button('Add Recipe')
-
     expect(page).to have_current_path(new_recipe_path)
   end
-  #   scenario 'user can see the list of recipes' do
-  #     user = create(:user)
-  #     recipe = create(name: 'Vegetable Soup', description: 'A delicious chicken curry', user: user)
-  #     visit recipe_index_path
-  #     expect(page).to have_content('Recipes')
-  #     expect(page).to have_content('Vegetable Soup')
-  #     expect(page).to have_content('A delicious chicken curry')
-  #     expect(page).to have_button('Remove')
-  #   end
 
-  #   scenario 'user sees a message when there are no recipes available' do
-  #     visit recipe_index_path
+  scenario 'user can remove their recipe' do
+    user = create(:user)
+    login_as(user, scope: :user)
+    visit recipe_index_path
+    expect(page).not_to have_button('Remove', exact: true)
+  end
 
-  #     expect(page).to have_content('Recipes')
-  #     expect(page).to have_content('No recipes available.')
-  #     expect(page).not_to have_button('Remove')
-  #   end
+  scenario 'user can add a new recipe' do
+    user = create(:user)
+    login_as(user, scope: :user)
+    visit recipe_index_path
+    expect(page).to have_button('Add Recipe', exact: true)
+  end
 end
