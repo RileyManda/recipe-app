49abe5cbb0406cfafebe923b016bb31f5d9705e8
RileyManda <rileymanda69@gmail.com>
2023-10-20 11:21:52 +0200

    Logic:Total value of food needed

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -3,14 +3,6 @@ class ShoppingController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_foods = @recipe.recipe_foods
     @total_items = @recipe_foods.sum(:quantity)
-    @total_value = 0
-
-    @recipe_foods.each do |recipe_food|
-      food = recipe_food.food
-      calculated_quantity = recipe_food.quantity - food.quantity
-      if calculated_quantity > 0
-        @total_value += calculated_quantity * food.price
-      end
-    end
+    @total_value = @recipe_foods.sum { |recipe_food| recipe_food.quantity * recipe_food.food.price }
   end
 end
