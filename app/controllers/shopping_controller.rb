9a1b1bbba96ef9e87a6a6060e7ea501746493eda
RileyManda <rileymanda69@gmail.com>
2023-10-20 11:02:45 +0200

    Shopping list logic and display

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -1,18 +1,16 @@
 class ShoppingController < ApplicationController
   def index
-    @recipe_foods = current_user.recipe_foods
-    @foods = Food.all
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_foods = @recipe.recipe_foods
     @total_items = 0
     @total_value = 0
 
     @recipe_foods.each do |recipe_food|
-      food = @foods.find_by(id: recipe_food.food_id)
-      if food
-        calculated_quantity = recipe_food.quantity - food.quantity
-        if calculated_quantity > 0
-          @total_items += calculated_quantity
-          @total_value += calculated_quantity * food.price
-        end
+      food = recipe_food.food
+      calculated_quantity = recipe_food.quantity - food.quantity
+      if calculated_quantity > 0
+        @total_items += calculated_quantity
+        @total_value += calculated_quantity * food.price
       end
     end
   end
