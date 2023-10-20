d9abf7ccac8ac85918626e4b5456217ac2624ed0
RileyManda <rileymanda69@gmail.com>
2023-10-20 11:18:31 +0200

    Calculate totla value of items against food

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -2,14 +2,13 @@ class ShoppingController < ApplicationController
   def index
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_foods = @recipe.recipe_foods
-    @total_items = 0
+    @total_items = @recipe_foods.sum(:quantity)
     @total_value = 0
 
     @recipe_foods.each do |recipe_food|
       food = recipe_food.food
       calculated_quantity = recipe_food.quantity - food.quantity
       if calculated_quantity > 0
-        @total_items += calculated_quantity
         @total_value += calculated_quantity * food.price
       end
     end
